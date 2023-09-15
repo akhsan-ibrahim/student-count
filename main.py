9278cb65043fceb4db096514381068c2b566d287
Akhsan Ibrahim <akhsanibra08@gmail.com>
2023-09-15 21:21:44 +0700

    define in or out

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 from ultralytics import YOLO
 from tracker import *
 
-model = YOLO('yolov8m.pt') # affect to accuracy of object when tracking
+model = YOLO('yolov8s.pt') # affect to accuracy of object when tracking
 
 # cursor coordinate
 def RGB(event,x,y,flags,param):
@@ -27,6 +27,9 @@ tracker = Tracker() # define object tracker
 
 area1 = [(494,289),(505,499),(578,496),(530,292)]
 area2 = [(548,290),(600,496),(637,493),(574,288)]
+
+going_out = {}
+going_in = {}
 
 while True:
   success,frame = cap.read()
@@ -53,28 +56,67 @@ while True:
     if 'person' in object_class: # only person class object
       list.append([x1, y1, x2, y2])
 
-    # detect with bbox
-    bbox_idx = tracker.update(list)
-    for bbox in bbox_idx:
-      x3,y3,x4,y4,id = bbox
-      # distance between object point and object polygon
-      result = cv2.pointPolygonTest(
+  # detect with bbox
+  bbox_idx = tracker.update(list)
+  for bbox in bbox_idx:
+    x3,y3,x4,y4,id = bbox
+
+    # (OBJECT GOING OUT) distance between object point and object polygon
+    result_out = cv2.pointPolygonTest(
+      np.array(area2,np.int32), # polygon area
+      ((x4,y4)), # object point
+      False # measure distance with polygon
+    )
+    # when object point on the polygon
+    if result_out >= 0:
+      going_out[id] = (x4,y4)
+    if id in going_out:
+      result_going_out = cv2.pointPolygonTest(
         np.array(area1,np.int32), # polygon area
         ((x4,y4)), # object point
         False # measure distance with polygon
       )
-      # when object point on the polygon
-      if result >= 0:
+      if result_going_out >= 0:
         cv2.circle(frame,(x4,y4),7,(255,0,255),-1) # show object point
         cv2.rectangle(frame,(x3,y3),(x4,y4),(255,255,255),2) # show object bbox
         cvzone.putTextRect(frame,f'{id}',(x3,y3),1,1) # show object label (id)
 
-  # print(list)
+    # (OBJECT GOING IN) distance between object point and object polygon
+    result_in = cv2.pointPolygonTest(
+      np.array(area1,np.int32), # polygon area
+      ((x4,y4)), # object point
+      False # measure distance with polygon
+    )
+    # when object point on the polygon
+    if result_in >= 0:
+      going_in[id] = (x4,y4)
+    if id in going_in:
+      result_going_in = cv2.pointPolygonTest(
+        np.array(area2,np.int32), # polygon area
+        ((x4,y4)), # object point
+        False # measure distance with polygon
+      )
+      if result_going_in >= 0:
+        cv2.circle(frame,(x4,y4),7,(255,0,255),-1) # show object point
+        cv2.rectangle(frame,(x3,y3),(x4,y4),(255,255,255),2) # show object bbox
+        cvzone.putTextRect(frame,f'{id}',(x3,y3),1,1) # show object label (id)
 
-  # set detection area
+
+  print("out:",going_out)
+  print("in:",going_in)
+
+  # set detection area 1
   cv2.polylines(
     frame, # drawing medium
     [np.array(area1,np.int32)], # change format fit to polylines format
+    True, # closed polygon --> every poin is connected
+    (0,255,0), # RGB color
+    2 # line weight
+  )
+  # set detection area 2
+  cv2.polylines(
+    frame, # drawing medium
+    [np.array(area2,np.int32)], # change format fit to polylines format
     True, # closed polygon --> every poin is connected
     (0,255,0), # RGB color
     2 # line weight
